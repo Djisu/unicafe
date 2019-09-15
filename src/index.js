972fdd8f3e06f2d4278396c8e32e258b118d6343
Djisu <pfleischer2002@yahoo.co.uk>
2019-09-14 19:00:14 -0700

    add statistics component commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+  // do not define a component within another component
+  const Statistics = (props) => {
+    return (
+        <div>
+            <strong>statistics</strong><br />
+            Good <span>{props.good}</span><br />
+            Neutral <span>{props.neutral}</span><br />
+            Bad <span>{props.bad}</span><br />
+            all <span>{props.good + props.bad + props.neutral}</span><br />
+            average  <span>{((props.good * 1) + (props.bad * -1) + (props.neutral * 0))/(props.good + props.bad + props.neutral)}</span><br />
+            positive  <span>{(props.good/(props.good + props.bad + props.neutral)) * 100} %</span><br /> 
+        </div>
+     )
+  }
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  } 
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  } 
+
   return (
     <div>
       {/* code here */}
       <p><strong>give feedback</strong></p><br />
 
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
 
-      <p>
-        <strong>statistics</strong><br />
-
-        Good <span>{good}</span><br />
-        Neutral <span>{neutral}</span><br />
-        Bad <span>{bad}</span><br />
-        all <span>{good + bad + neutral}</span><br />
-        average  <span>{((good * 1) + (bad * -1) + (neutral * 0))/(good + bad + neutral)}</span><br />
-        positive  <span>{(good/(good + bad + neutral)) * 100}</span><br />
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
 
     </div>
   )
